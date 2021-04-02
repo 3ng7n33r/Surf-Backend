3faf258152453985d0ee02a569349d935b932603
Max Blacher-L <m.blacher@web.de>
2021-04-02 15:15:23 +0200

    django-heroku

--- Surf_Backend/settings.py
+++ Surf_Backend/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,4 +130,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Activate Django-Heroku.
+# Configure Django App for Heroku.
 django_heroku.settings(locals())
